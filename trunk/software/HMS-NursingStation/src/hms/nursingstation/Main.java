557ee5eda2f67e2dd6afd20668489dcc6ff7f908
ptr5201@g.rit.edu <ptr5201@g.rit.edu@aeb869f2-e085-74be-3897-70857bf9dfa6>
2012-02-15 18:18:47 +0000

    Basic implementation to have the client find the server object in the RMI registry.

--- trunk/software/HMS-NursingStation/src/hms/nursingstation/Main.java
+++ trunk/software/HMS-NursingStation/src/hms/nursingstation/Main.java
@@ -1,13 +1,33 @@
 package hms.nursingstation;
 
+import hms.common.Monitor;
+
+import java.rmi.NotBoundException;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Main {
+	
+	static final String BEDSIDE_SERVER_NAME = "hms.bedsidemonitor";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.setSecurityManager(new RMISecurityManager());
+		Monitor server;
+		
+		try {
+			Registry registry = LocateRegistry.getRegistry();
+			server = (Monitor)registry.lookup(BEDSIDE_SERVER_NAME);
+			System.out.println("Nursing Station found a Bedside Monitor Server.");
+		} catch (RemoteException re) {
+			re.printStackTrace();
+		} catch (NotBoundException nbe) {
+			nbe.printStackTrace();
+		}
 	}
 
 }
