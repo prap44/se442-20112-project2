406785536c7817b63f009e45ea48b341c6f2997b
ptr5201 <ptr5201@g.rit.edu>
2012-02-15 18:00:13 +0000

    Basic implementation to get server and RMI registry up and running

--- software/HMS-BedsideMonitor/src/hms/bedsidemonitor/Main.java
+++ software/HMS-BedsideMonitor/src/hms/bedsidemonitor/Main.java
@@ -1,13 +1,40 @@
 package hms.bedsidemonitor;
 
+import hms.common.Monitor;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Monitor server = null;
+		
+		try {
+			// Create the server hosting the bedside monitor
+			server = new MonitorImpl();
+			
+			// Export the object to RMI
+			UnicastRemoteObject.exportObject(server);
+		} catch (RemoteException re) {
+			re.printStackTrace();
+		}
+		
+		// Register an external name for the service
+		try {
+			Naming.rebind("hms.bedsidemonitor", server);
+		} catch (RemoteException re) {
+			re.printStackTrace();
+		} catch (MalformedURLException murle) {
+			murle.printStackTrace();
+		}
+		
+		System.out.println("Server started.");
 	}
 
 }
