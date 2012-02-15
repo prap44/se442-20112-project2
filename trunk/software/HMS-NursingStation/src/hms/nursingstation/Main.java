4d8b7c30497aff8caabdb14c57613ffc075067a9
ptr5201@g.rit.edu <ptr5201@g.rit.edu@aeb869f2-e085-74be-3897-70857bf9dfa6>
2012-02-15 18:34:02 +0000

    Added notes on how server and client should communicate between each other. Summary: subscribe/notify

--- trunk/software/HMS-NursingStation/src/hms/nursingstation/Main.java
+++ trunk/software/HMS-NursingStation/src/hms/nursingstation/Main.java
@@ -23,6 +23,12 @@ public class Main {
 			Registry registry = LocateRegistry.getRegistry();
 			server = (Monitor)registry.lookup(BEDSIDE_SERVER_NAME);
 			System.out.println("Nursing Station found a Bedside Monitor Server.");
+			
+			// subscribe to B.S.M. server object.
+			// server will handle request that client wants updates from it.
+			// server will maintain a list of clients to send updates to.
+			// once the server gets its patient data, it sends that data in events
+			// to its list of clients that are subscribed to it.
 		} catch (RemoteException re) {
 			re.printStackTrace();
 		} catch (NotBoundException nbe) {
