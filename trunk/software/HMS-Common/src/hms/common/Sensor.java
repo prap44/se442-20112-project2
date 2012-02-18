9a4028d5d15be6e08b77390639737fff23374397
pxm9906@g.rit.edu <pxm9906@g.rit.edu@aeb869f2-e085-74be-3897-70857bf9dfa6>
2012-02-18 19:55:44 +0000

    added several new variables with getters/setters, the vitalChangeMethod as well as the vitalSpike and SpikeCheck helper methods. Sensors now start at a configured variable and will fluctuate around the number, randomly spiking in order to produce results that may cause an alarm. After a certain amount of time outside of a range from the initial bound the value will reset to stabilize the readouts.

--- trunk/software/HMS-Common/src/hms/common/Sensor.java
+++ trunk/software/HMS-Common/src/hms/common/Sensor.java
@@ -1,14 +1,24 @@
 package hms.common;
 
+import java.util.Random;
+
 public class Sensor {
 	private String name;
 	private double scalar;
 	private double offset;
+	private Random randomNumber;
+	private boolean spikeFlag;
+	private int currentValue;
+	private int varianceCount;
+	private final int sensorInitialValue;
 	
-	public Sensor(String name, double scalar, double offset) {
+	public Sensor(String name, double scalar, double offset, int sensorInitial) {
 		this.name = name;
 		this.scalar = scalar;
 		this.offset = offset;
+		this.randomNumber = new Random();
+		this.currentValue = sensorInitial;
+		this.sensorInitialValue = sensorInitial;
 	}
 	
 	public void setName(String name) {
@@ -35,7 +45,97 @@ public class Sensor {
 		return this.offset;
 	}
 	
+	public int getCurrentValue(){
+		return this.currentValue;
+	}
+	public void setCurrentValue(int value){
+		this.currentValue = value;
+	}
+	public boolean getSpikeFlag(){
+		return this.spikeFlag;
+	}
+	public void setSpikeFlag(boolean flag){
+		this.spikeFlag = flag;
+	}
+	
+	public int getSensorInitialValue(){
+		return this.sensorInitialValue;
+	}
+	 
 	public int convert(double value) {
 		return (int)Math.round(value * this.scalar + this.offset);
+	}
+	
+	private double vitalChange(){
+		double rawData;
+		double convert = 100;
+		spikeCheck();
+		if( randomNumber.nextInt()%17 == 0){
+			this.vitalSpike();
+			//System.out.println("Vital: " + name + " spiked");
+		}
+		if (this.getSpikeFlag() == false){
+			if(randomNumber.nextInt()%2 == 0){
+				currentValue = currentValue + (randomNumber.nextInt(3));
+				varianceCount = 0;
+			}
+			else{
+				currentValue = currentValue - (randomNumber.nextInt(3));
+				varianceCount = 0;
+			}
+		}
+		else{
+			if(randomNumber.nextInt()%2 == 0){
+				currentValue = currentValue + (randomNumber.nextInt(2)+randomNumber.nextInt(6));
+				varianceCount++;
+			}
+			else{
+				currentValue = currentValue - (randomNumber.nextInt(2)+randomNumber.nextInt(6));
+				varianceCount++;
+			
+			}
+		}
+			
+			this.setSpikeFlag(false);
+			rawData = this.getCurrentValue()/convert;
+			System.out.println("VitalSign " +  this.getName() + " raw data read: " +  rawData);
+			return rawData;
+	}
+	private void vitalSpike(){
+		if(currentValue < sensorInitialValue){
+			currentValue = currentValue - 9;
+		}
+		else{
+			currentValue = currentValue + 9;
+		}
+	}
+	
+	private void spikeCheck(){
+		if(varianceCount == 10){
+			this.setCurrentValue(this.getSensorInitialValue());
+			varianceCount = 0;
+		}
+		else if( currentValue <= this.getSensorInitialValue()-10 || currentValue >= this.getSensorInitialValue()+10){
+			this.setSpikeFlag(true);
+		}
+	}
+	public static void main(String[] args){
+		Runnable r = new Runnable(){
+			public void run(){
+				Sensor dummy = new Sensor("Blood Pressure",0.5,1,50);
+				while (true) {
+					dummy.vitalChange();
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+			}
+		};
+		Thread t = new Thread(r);
+		t.run();
 	}
 }
